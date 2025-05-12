f3e15b52a4ed5ae98cc8d24c8f1eae4645cb6384
Maj Uran <uran.maj@gmail.com>
2025-05-12 23:02:29 +0200

    Dark mode based on system

--- assets/js/mode-switch.js
+++ assets/js/mode-switch.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const KEY = 'dark';
     const button = document.getElementById('mode-switch');
 
-    if (localStorage.getItem('dark')) {
-        document.body.classList.add('dark');
+    const userPref = localStorage.getItem(KEY);
+    if (userPref === 'true') {
+        document.body.classList.add(KEY);
+    } else if (userPref === null && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.add(KEY);
     }
 
     button.addEventListener('click', function () {
-        const isDark = document.body.classList.toggle('dark');
-        if (isDark) {
-            localStorage.setItem('dark', 'true');
-        } else {
-            localStorage.removeItem('dark');
-        }
+        const isDark = document.body.classList.toggle(KEY);
+        localStorage.setItem(KEY, isDark ? 'true' : 'false');
     });
 });
